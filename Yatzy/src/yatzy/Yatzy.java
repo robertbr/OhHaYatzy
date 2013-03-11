136a7a2b117bd992a97613a63e6ec2179ef9494f
robertbr <robertbrunberg@hotmail.com>
2013-03-11 20:27:01 +0200

    first draft

---
+++ Yatzy/src/yatzy/Yatzy.java
@@ -0,0 +1,22 @@
+// 4. harjoitukset, tehtävä , Robert Brunberg
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package yatzy;
+
+/**
+ *
+ * @author Robert
+ */
+public class Yatzy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}

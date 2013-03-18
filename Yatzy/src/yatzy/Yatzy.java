830b03ff63770c49e4f1fe59d95220458b8f4790
Robert H A Brunberg <robertbrunberg@hotmail.com>
2013-03-18 15:34:42 +0200

    Yatzy

--- Yatzy/src/yatzy/Yatzy.java
+++ Yatzy/src/yatzy/Yatzy.java
@@ -17,11 +17,5 @@ public class Yatzy {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Die noppa1 = new Die();
-        System.out.println(noppa1.getValue());
-        
-        ScoreCalculator calc = new ScoreCalculator(6,6,4,6,6);
-        System.out.println(calc.UpperSection(6));
     }
-
 }

c6c2ed63a70b24c2bd7e8eb1af7bd519a9dee1ff
Robert H A Brunberg <robertbrunberg@hotmail.com>
2013-03-18 15:48:46 +0200

    Yatzy

---
+++ Yatzy/test/DieTest.java
@@ -0,0 +1,63 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import yatzy.Die;
+
+/**
+ *
+ * @author robertbr
+ */
+public class DieTest {
+    
+    Die die;
+    
+    public DieTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    
+    public void testDieValueBetweenOneAndSix(){
+        Boolean dieValueBetweenOneAndSix = true;
+        //We throw the die 1000 times to check, then we know that it is probably true
+        int i = 0;
+        while(i<1000){
+            die = new Die();
+            if(die.getValue()<1 || die.getValue()>6){
+                dieValueBetweenOneAndSix = false;
+            }
+            i++;
+        }
+        assertEquals(true,dieValueBetweenOneAndSix);
+        
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}

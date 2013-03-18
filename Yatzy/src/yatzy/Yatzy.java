ada00d0fd5475f5d70f1b64db7e062431685def8
Robert H A Brunberg <robertbrunberg@hotmail.com>
2013-03-18 12:58:18 +0200

    Yatzy

--- Yatzy/src/yatzy/Yatzy.java
+++ Yatzy/src/yatzy/Yatzy.java
@@ -1,4 +1,4 @@
-// 4. harjoitukset, tehtävä , Robert Brunberg
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -17,6 +17,11 @@ public class Yatzy {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Die noppa1 = new Die();
+        System.out.println(noppa1.getValue());
+        
+        ScoreCalculator calc = new ScoreCalculator(6,6,4,6,6);
+        System.out.println(calc.UpperSection(6));
     }
 
 }
